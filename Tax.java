84ab14016147d6041c911a132069a8cc8df2a845
Ntando-star <69920434+Ntando-star@users.noreply.github.com>
2021-07-15 10:32:15 +0200

    Add files via upload

---
+++ Tax.java
@@ -0,0 +1,14 @@
+package com.psybergate.vacwork_202107;
+
+import java.util.Scanner;
+
+public class Tax {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int i = scan.nextInt();
+    System.out.println("number : "+i);
+
+  }
+
+}
