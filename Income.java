799d05469ccf150154d3a5af1594181e1d51b01a
Ntando-star <69920434+Ntando-star@users.noreply.github.com>
2021-07-15 12:45:25 +0200

    Update Income.java

--- Income.java
+++ Income.java
@@ -1,4 +1,13 @@
 package com.psybergate.vacwork_202107;
 
 public class Income {
+   public static double getIncome(double salary, double bonus, double interest, double gains) {
+
+    interest = interest - 23800;
+    gains = (gains - 40000) * 0.4;
+
+    double income = salary + bonus + interest + gains;
+    return income;
+  }
+
 }
