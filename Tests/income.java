2053d94e2432268444e55a829272761c49dad759
Ntando-star <69920434+Ntando-star@users.noreply.github.com>
2021-07-19 23:16:47 +0200

    Update income.java

--- Tests/income.java
+++ Tests/income.java
@@ -8,10 +8,10 @@ public class income {
   @Test
   public void testInterest(){
     Income income = new Income();
-    income.getIncome(500000,25000,23001,800000);
+    income.getIncome(500000,25000,23801,800000);
     assertEquals(1,income.interest);
 
-    income.getIncome(500000,25000,23000,800000);
+    income.getIncome(500000,25000,23800,800000);
     assertEquals(0,income.interest);
 
     income.getIncome(500000,25000,20000,800000);
@@ -20,11 +20,24 @@ public class income {
 
   @Test
   public void testGains(){
+    Income income = new Income();
+    income.getIncome(500000,25000,23800,800000);
+    assertEquals(304000,income.gains);
 
+    income.getIncome(500000,25000,23800,40001);
+    assertEquals(0,income.gains);
+
+    income.getIncome(500000,25000,23800,40002);
+    assertEquals(1,income.gains);
   }
 
   @Test
   public void testIncome(){
+    Income income = new Income();
+    income.getIncome(500000,25000,45000,800000);
+    assertEquals(850200,income.gains);
 
+    income.getIncome(500000,0,0,0);
+    assertEquals(500000,income.gains);
   }
 }
